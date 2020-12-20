75cb2c3e08710691379e32b17da3594883607d87
purusottam <purushottam@nimapinfotech.com>
2020-12-20 19:06:40 +0530

    add dynamic input in HookBookContainer components

--- src/components/HookBookContainer.jsx
+++ src/components/HookBookContainer.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { buyBook } from "../redux/Index";
 import { useSelector, useDispatch } from "react-redux"; // using this we can extract data from Redux store
 const BookContainer = (props) => {
     const numberOfBooks = useSelector(state => state.numberOfBooks);
     const dispatch = useDispatch()
-
+    const [number, setNumber] = useState(1);
+    const onChangeEvent = (OnChangevalue) => {
+        setNumber(OnChangevalue.target.value)
+    }
     return (
         <>
             <h1>Hook container</h1>
             <h1>Number of Books - {numberOfBooks}</h1>
-            <button onClick={()=>dispatch(buyBook())}>Buy Book</button>
+            <input type="text" value={number} onChange={onChangeEvent} />
+            <button onClick={() => dispatch(buyBook(number))}>Buy {number} Book</button>
         </>
     )
 }
